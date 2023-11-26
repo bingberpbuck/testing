6324a3e71366c47526a7bd75cb54414c1bc0e2f6
David Lee <davidlee@Davids-MacBook-Pro-88.local>
2023-11-25 20:02:06 -0500

    hi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
+import TodoForm from "./components/TodoForm";
+import React from "react";
+import "./App.css";
+import {useState} from "react";
 import logo from './logo.svg';
-import './App.css';
+import TodoList from "./components/TodoList";
+
 
 function App() {
+  const[todos, setTodos] = useState([])
+
+  function addTodo(todo) {
+    setTodos([todo, ...todos]);
+  }
+
+
+ 
+  function toggleComplete(id) {
+    let newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        };
+      }
+      return todo;
+    })
+    setTodos(newTodos); 
+    console.log(newTodos)
+  }
+
+  function removeTodo(id) {
+    const newTodos = todos.filter((item) => item.id != id)
+         setTodos(newTodos)
+         console.log(newTodos)
+      }
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className = "App">
+          <header className = "App-header">
+              <p>React Todo</p>
+              <TodoForm addTodo={addTodo}/>
+              <TodoList
+                todos={todos}
+                removeTodo={removeTodo}
+                toggleComplete={toggleComplete}
+              />
+          </header>
+      </div>
   );
 }
+
 
 export default App;
